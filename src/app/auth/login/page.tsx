41ba5f65861712a88a0e6a8bd36930011ad74717
Tiago Navarro <130316709+NTiago-s@users.noreply.github.com>
2024-09-15 19:32:07 -0300

    update

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 import { LoginAction, LoginGoogleAction } from "@/actions/auth/auth-actions";
-import Input from "@/components/input/inputLogin";
+import Input from "@/components/ui/inputLogin";
 import React from "react";
 import { useForm } from "react-hook-form";
 import { useRouter } from "next/navigation";
 import { alerts } from "@/utils/alerts";
-
+import { Button } from "@/components/ui/button";
 const Login = () => {
   const { register, handleSubmit } = useForm();
   const router = useRouter();
@@ -42,11 +42,21 @@ const Login = () => {
       <button
         type="button"
         onClick={SubmitGoogle}
-        className="bg-slate-600 p-3 rounded-lg"
+        className="flex justify-center w-full items-center gap-2 bg-white p-3 rounded-lg"
+        aria-label="Iniciar sesión con Google"
       >
-        Iniciar Sesión con Google
+        <img
+          src="/Google__G__logo.svg.webp"
+          alt="google-icon"
+          className="size-8"
+        />
+        Iniciar con Google
       </button>
-
+      <div className="flex items-center gap-4">
+        <hr className="flex-grow border-gray-400" />
+        <p className="text-white">O</p>
+        <hr className="flex-grow border-gray-400" />
+      </div>
       <Input type="text" placeholder="Email" name="email" register={register} />
       <Input
         type="password"
@@ -54,9 +64,20 @@ const Login = () => {
         name="password"
         register={register}
       />
-      <button type="submit" className="bg-slate-600 p-3 rounded-lg">
-        Iniciar Sesión
-      </button>
+      <div className="flex justify-center items-center text-white gap-2 ">
+        <input type="checkbox" className="cursor-pointer size-4" />
+        <p>Acepto los terminos y politicas de la poronga esta</p>
+      </div>
+      <Button txt="Iniciar Sesión" />
+      <div className="flex flex-col justify-center items-center gap-4 text-white">
+        <a>¿Has olvidado tu contraseña?</a>
+        <p>
+          Todavia no tienes una cuenta?{" "}
+          <a href="/auth/register" className="underline">
+            Registrate
+          </a>
+        </p>
+      </div>
     </form>
   );
 };
--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,3 +1,81 @@
-export default function Register() {
-  return <h1>register page</h1>;
-}
+"use client";
+import { LoginAction, LoginGoogleAction } from "@/actions/auth/auth-actions";
+import Input from "@/components/ui/inputLogin";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import { alerts } from "@/utils/alerts";
+import { Button } from "@/components/ui/button";
+const Register = () => {
+  const { register, handleSubmit } = useForm();
+  const router = useRouter();
+
+  const onSubmit = async (data: any) => {
+    const result = await LoginAction(data);
+
+    if (result?.error || !result || result.status === 401) {
+      alerts(
+        "error",
+        typeof result?.error === "string"
+          ? result?.error
+          : JSON.stringify(result?.error)
+      );
+    } else if (result.status === 200) {
+      alerts("success", "Inicio de Sesion correcto");
+      router.push("/");
+    }
+  };
+
+  const SubmitGoogle = async () => {
+    const result = await LoginGoogleAction();
+    console.log(result);
+    if (result) {
+      router.push("/");
+    }
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex flex-col gap-4 p-8 border border-white justify-center align-middle"
+    >
+      <button
+        type="button"
+        onClick={SubmitGoogle}
+        className="flex justify-center w-full items-center gap-2 bg-white p-3 rounded-lg"
+        aria-label="Iniciar sesión con Google"
+      >
+        <img
+          src="/Google__G__logo.svg.webp"
+          alt="google-icon"
+          className="size-8"
+        />
+        Iniciar con Google
+      </button>
+      <div className="flex items-center gap-4">
+        <hr className="flex-grow border-gray-400" />
+        <p className="text-white">O</p>
+        <hr className="flex-grow border-gray-400" />
+      </div>
+      <Input type="text" placeholder="Email" name="email" register={register} />
+      <Input
+        type="password"
+        placeholder="Contraseña"
+        name="password"
+        register={register}
+      />
+      <Button txt="Registrarte" />
+      <div className="flex flex-col justify-center items-center gap-4 text-white">
+        <a>¿Has olvidado tu contraseña?</a>
+        <p>
+          Todavia no tienes una cuenta?{" "}
+          <a href="/auth/register" className="underline">
+            Registrate
+          </a>
+        </p>
+      </div>
+    </form>
+  );
+};
+
+export default Register;
