7e3c61228e361eb90343eceeb83bc52bd8022dee
Tiago Navarro <130316709+NTiago-s@users.noreply.github.com>
2025-01-02 11:28:53 -0300

    create component of mp

--- src/components/method-payment/index.tsx
+++ src/components/method-payment/index.tsx
@@ -1,3 +1,10 @@
-export default function MercadoLibre() {
-  return <div></div>;
+import SectionContainer from "../yourcard-landing/section-container";
+import MercadoPago from "./mercado-pago";
+
+export default function PaymenthMethod() {
+  return (
+    <SectionContainer>
+      <MercadoPago />
+    </SectionContainer>
+  );
 }
