dd052db5eaad0eefb8af4ed8cee4b64b314ae9e7
Tiago Navarro <130316709+NTiago-s@users.noreply.github.com>
2025-01-03 10:54:31 -0300

    update mp

--- src/components/method-payment/mercado-pago/index.tsx
+++ src/components/method-payment/mercado-pago/index.tsx
@@ -1,3 +1,13 @@
 export default function MercadoPago() {
-  return <div></div>;
+  return (
+    <div>
+      <a
+        href="https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=2c938084942a912f01942c68783d006b"
+        className="relative hover:opacity-70 transition duration-300 inline-flex items-center justify-center p-2 w-full mb-2 me-2 overflow-hidden text-sm font-medium  rounded-lg group bg-gradient-to-br to-orange-400 from-pink-500  text-white  focus:ring-4 focus:outline-none focus:ring-pink-800"
+        target="_blank"
+      >
+        Suscribirte
+      </a>
+    </div>
+  );
 }
