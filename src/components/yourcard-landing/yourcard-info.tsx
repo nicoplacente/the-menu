3703b9d7b1defe899466bb3aff7cadd33784bb85
nicoplacente <nicolasplacente@gmail.com>
2025-06-09 14:08:32 -0300

    add Link in yourcard-info

--- src/components/yourcard-landing/yourcard-info.tsx
+++ src/components/yourcard-landing/yourcard-info.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function YourcardInfo() {
   return (
     <article className="flex xl:items-center gap-12 xl:gap-0 xl:flex-row flex-col xl:justify-evenly">
@@ -54,11 +56,15 @@ export default function YourcardInfo() {
           <p>La carta que transforma tu negocio.</p>
         </li>
 
-        <button className="relative animate-pulse hover:opacity-70 transition duration-300 inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium  rounded-lg group bg-gradient-to-br to-orange-400 from-pink-500  text-white  focus:ring-4 focus:outline-none focus:ring-pink-800">
+        <Link
+          href="/create-card"
+          aria-label="Crear carta"
+          className="relative animate-pulse hover:opacity-70 transition duration-300 inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium  rounded-lg group bg-gradient-to-br to-orange-400 from-pink-500  text-white  focus:ring-4 focus:outline-none focus:ring-pink-800"
+        >
           <span className="relative flex items-center gap-2 px-5 py-2.5 transition-all ease-in duration-75 rounded-md">
             ¡Empieza hoy mismo y genera una impresión inolvidable!
           </span>
-        </button>
+        </Link>
       </ul>
     </article>
   );
